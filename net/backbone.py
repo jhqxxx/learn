322f5b44c218fdbc495ad785348d1a237a973ea7
hqjiang <1614813726@qq.com>
2022-09-26 23:22:06 +0800

    add resnet

--- net/backbone.py
+++ net/backbone.py
@@ -1,3 +1,13 @@
+'''
+Descripttion: 
+version: 
+Author: jhq
+Date: 2022-09-25 14:16:51
+LastEditors: jhq
+LastEditTime: 2022-09-26 23:21:22
+'''
+from turtle import forward
+from xml.etree.ElementInclude import include
 import torch
 import torch.nn as nn
 
@@ -46,5 +56,133 @@ class VGG(nn.Module):
         x = self.fc(x)
         return x
 
-model = VGG(num_classes=10)
+
+def conv3x3(in_channels, out_channels, stride=1, padding=1, groups=1, dilation=1):
+    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, 
+                    stride=stride, padding=padding, groups=groups, bias=False, dilation=dilation)
+
+def conv1x1(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_channel, out_channel, stride=1, padding=1, downsample=None) -> None:
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels=in_channel, out_channels=out_channel,
+                        stride=stride,padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(in_channels=out_channel, out_channels=out_channel,
+                        stride=stride, padding=padding)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+        self.downsample = downsample
+    
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+        x += identity
+        x = self.relu(x)
+        return x
+
+class Bottleneck(nn.Module):
+    expansion = 4
+    def __init__(self, in_channel, mid_channel, stride=1, padding=1, downsample=None) -> None:
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv1x1(in_channels=in_channel, out_channels=mid_channel)    
+        self.bn1 = nn.BatchNorm2d(mid_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(in_channels=mid_channel, out_channels=mid_channel, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(mid_channel)
+        self.conv3 = conv1x1(in_channels=mid_channel, out_channels=mid_channel*self.expansion)
+        self.bn3 = nn.BatchNorm2d(mid_channel*self.expansion)
+        self.downsample = downsample
+
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+        x += identity
+        x = self.relu(x)
+        return x
+        
+
+class ResNet(nn.Module):
+    def __init__(self, block, block_list, num_classes, include_top=True) -> None:
+        super(ResNet, self).__init__()
+        self.include_top = include_top
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.in_channels, kernel_size=7, 
+                                stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self.make_layer(block=block, channel=64, block_list=block_list[0])
+        self.layer2 = self.make_layer(block=block, channel=128, block_list=block_list[1], stride=2)
+        self.layer3 = self.make_layer(block=block, channel=258, block_list=block_list[2], stride=2)
+        self.layer4 = self.make_layer(block=block, channel=512, block_list=block_list[3], stride=2)
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool1d((1, 1))
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+    
+    def make_layer(self, block, channel, block_list, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, channel*block.expansion,
+                            kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channel*block.expansion)
+            )
+        
+        layers = []
+        layers.append(block(self.in_channels, channel, stride=stride, downsample=downsample))
+        self.in_channels = channel * block.expansion
+        for _ in range(1, block_list):
+            layers.append(block(self.in_channels, channel))
+        
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x, 1)
+            x = self.fc(x)
+        
+        return x
+
+
+def get_resnet(layer_num, num_classes=1000, include_top=True):
+    if layer_num == 18:
+        return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=num_classes, include_top=include_top)
+    elif layer_num == 34:
+        return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+    elif layer_num == 50:
+        return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
+    elif layer_num == 101:
+        return ResNet(Bottleneck, [3, 4, 23, 3], num_classes=num_classes, include_top=include_top)
+    elif layer_num == 152:
+        return ResNet(Bottleneck, [3, 8, 36, 3], num_classes=num_classes, include_top=include_top)
+    else:
+        return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=num_classes, include_top=include_top)
+
+# model = VGG(num_classes=10)
+model = get_resnet(50, 10)
 print(model)
