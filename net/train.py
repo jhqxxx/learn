a00922b9322079983c2cc6fbf51672342ee4635d
jhqxxx <18280426169@163.com>
2022-10-21 17:33:42 +0800

    update train about

--- net/train.py
+++ net/train.py
@@ -1,257 +1,89 @@
-'''
-Descripttion: 
-version: 
-Author: jhq
-Date: 2022-09-25 20:10:09
-LastEditors: jhq
-LastEditTime: 2022-09-25 21:26:01
-'''
-from __future__ import print_function, division
 
+
+import argparse
+from ast import arg
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.autograd import Variable
-import numpy as np
-from torchvision import models
-import matplotlib as plt
-
-batch_size = 16
-learning_rate = 0.0002
-epoch = 10
-
-train_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((.5, .5, .5), (.5, .5, .5))
-])
-val_transforms = transforms.Compose([
-    transforms.Resize(256),
-    transforms.RandomResizedCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize((.5, .5, .5), (.5, .5, .5))
-])
-
-train_dir = './VGGDataSet/train'
-train_datasets = datasets.ImageFolder(train_dir, transform=train_transforms)
-train_dataloader = torch.utils.data.DataLoader(train_datasets, batch_size=batch_size, shuffle=True)
-
-val_dir = './VGGDataSet/val'
-val_datasets = datasets.ImageFolder(val_dir, transform=val_transforms)
-val_dataloader = torch.utils.data.DataLoader(val_datasets, batch_size=batch_size, shuffle=True)
+from utils import WarmUpLR
+import os
 
 
-class VGGNet(nn.Module):
-    def __init__(self, num_classes=3):
-        super(VGGNet, self).__init__()
-        net = models.vgg16(pretrained=True)
-        net.classifier = nn.Sequential()
-        self.features = net
-        self.classifier = nn.Sequential(
-                nn.Linear(512 * 7 * 7, 512),
-                nn.ReLU(True),
-                nn.Dropout(),
-                nn.Linear(512, 128),
-                nn.ReLU(True),
-                nn.Dropout(),
-                nn.Linear(128, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
-#--------------------训练过程---------------------------------
-model = VGGNet()
-if torch.cuda.is_available():
-    model.cuda()
-params = [{'params': md.parameters()} for md in model.children()
-          if md in [model.classifier]]
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-loss_func = nn.CrossEntropyLoss()
-
-Loss_list = []
-Accuracy_list = []
-
-
-
-for epoch in range(100):
-    print('epoch {}'.format(epoch + 1))
-    # training-----------------------------
-    train_loss = 0.
-    train_acc = 0.
-    for batch_x, batch_y in train_dataloader:
-        batch_x, batch_y = Variable(batch_x).cuda(), Variable(batch_y).cuda()
-        out = model(batch_x)
-        loss = loss_func(out, batch_y)
-        train_loss += loss.data[0]
-        pred = torch.max(out, 1)[1]
-        train_correct = (pred == batch_y).sum()
-        train_acc += train_correct.data[0]
+def train(epoch):
+    net.train()
+    for batch_index, (images, labels) in enumerate(training_loader):
+        if epoch <= args.warm:
+            warmup_scheduler.step()
+        
+        images = images.cuda()
+        labels = labels.cuda()
         optimizer.zero_grad()
+        outputs = net(images)
+        loss = loss_function(outputs, labels)
         loss.backward()
         optimizer.step()
-    print('Train Loss: {:.6f}, Acc: {:.6f}'.format(train_loss / (len(
-        train_datasets)), train_acc / (len(train_datasets))))
 
-    # evaluation--------------------------------
-    model.eval()
-    eval_loss = 0.
-    eval_acc = 0.
-    for batch_x, batch_y in val_dataloader:
-        batch_x, batch_y = Variable(batch_x, volatile=True).cuda(), Variable(batch_y, volatile=True).cuda()
-        out = model(batch_x)
-        loss = loss_func(out, batch_y)
-        eval_loss += loss.data[0]
-        pred = torch.max(out, 1)[1]
-        num_correct = (pred == batch_y).sum()
-        eval_acc += num_correct.data[0]
-    print('Test Loss: {:.6f}, Acc: {:.6f}'.format(eval_loss / (len(
-        val_datasets)), eval_acc / (len(val_datasets))))
-    
-    Loss_list.append(eval_loss / (len(val_datasets)))
-    Accuracy_list.append(100 * eval_acc / (len(val_datasets)))
-
-x1 = range(0, 100)
-x2 = range(0, 100)
-y1 = Accuracy_list
-y2 = Loss_list
-plt.subplot(2, 1, 1)
-plt.plot(x1, y1, 'o-')
-plt.title('Test accuracy vs. epoches')
-plt.ylabel('Test accuracy')
-plt.subplot(2, 1, 2)
-plt.plot(x2, y2, '.-')
-plt.xlabel('Test loss vs. epoches')
-plt.ylabel('Test loss')
-plt.show()
-# plt.savefig("accuracy_loss.jpg")
+        print(f'Training Epoch:{epoch}[{batch_index*args.batch_size+len(images)}/{len(training_loader.dataset)}]\t \
+                    Loss:{loss.item()}, LR:{optimizer.param_groups[0]["lr"]}')
 
 
-from nlp.task.CIFAR10_try.ResNet import ResNet34	# ResNet本地导入，就是上面的网络模型导入
-import os
-import json
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import transforms, datasets
-from tqdm import tqdm
-def main():
-    # 判断cuda? cpu?
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using {} device.".format(device))
-    
-    # 数据处理
-    data_transform = {
-        "train": transforms.Compose([transforms.RandomResizedCrop(224),
-                                     transforms.RandomHorizontalFlip(),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
-        "val": transforms.Compose([transforms.Resize(256),
-                                   transforms.CenterCrop(224),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
-	# 设置路径
-    data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
-    image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
-    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
-    # 数据导入
-    train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
-                                         transform=data_transform["train"])
-    train_num = len(train_dataset)
-
-    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
-    flower_list = train_dataset.class_to_idx
-    cla_dict = dict((val, key) for key, val in flower_list.items())
-    # write dict into json file
-    json_str = json.dumps(cla_dict, indent=4)
-    with open('class_indices.json', 'w') as json_file:
-        json_file.write(json_str)
-
-    batch_size = 16
-    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
-    print('Using {} dataloader workers every process'.format(nw))
-
-    train_loader = torch.utils.data.DataLoader(train_dataset,
-                                               batch_size=batch_size, shuffle=True,
-                                               num_workers=nw)
-
-    validate_dataset = datasets.ImageFolder(root=os.path.join(image_path, "val"),
-                                            transform=data_transform["val"])
-    val_num = len(validate_dataset)
-    validate_loader = torch.utils.data.DataLoader(validate_dataset,
-                                                  batch_size=batch_size, shuffle=False,
-                                                  num_workers=nw)
-
-    print("using {} images for training, {} images for validation.".format(train_num,
-                                                                           val_num))
-
-    net = ResNet34()
-    model_weight_path = "./resnet34-pre.pth"	# 官网权重：https://download.pytorch.org/models/resnet34-333f7ec4.pth
-    assert os.path.exists(model_weight_path), "file {} does not exist.".format(model_weight_path)
-    net.load_state_dict(torch.load(model_weight_path, map_location=device))
-    in_channel = net.fc.in_features
-    net.fc = nn.Linear(in_channel, 5)
-    net.to(device)
-
-    # 优化器
-    loss_function = nn.CrossEntropyLoss()
-    params = [p for p in net.parameters() if p.requires_grad]
-    optimizer = optim.Adam(params, lr=0.0001)
-
-    epochs = 5
-    best_acc = 0.0
-    save_path = './resNet34.pth'
-    train_steps = len(train_loader)
-    for epoch in range(epochs):
-        # train
-        net.train()
-        running_loss = 0.0
-        train_bar = tqdm(train_loader)
-        for step, data in enumerate(train_bar):
-            images, labels = data
-            optimizer.zero_grad()
-            logits = net(images.to(device))
-            loss = loss_function(logits, labels.to(device))
-            loss.backward()
-            optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-
-            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
-
-        # validate
-        net.eval()
-        acc = 0.0  # accumulate accurate number / epoch
-        with torch.no_grad():
-            val_bar = tqdm(validate_loader)
-            for val_data in val_bar:
-                val_images, val_labels = val_data
-                outputs = net(val_images.to(device))
-                # loss = loss_function(outputs, test_labels)
-                predict_y = torch.max(outputs, dim=1)[1]
-                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
-
-                val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
-                                                           epochs)
-
-        val_accurate = acc / val_num
-        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
-              (epoch + 1, running_loss / train_steps, val_accurate))
-
-        if val_accurate > best_acc:
-            best_acc = val_accurate
-            torch.save(net.state_dict(), save_path)
-
-    print('Finished Training')
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--net', type=str, required=True, help='net type')
+    parser.add_argument('--gpu', action='store_true', default=False, help='use gpu or not')
+    parser.add_argument('--batch-size', type=int, default=64, help='batch size of dataloader')
+    parser.add_argument('--warm', type=int, default=1, help='warm up training phase')
+    parser.add_argument('--lr', type=float, default=0.1, help='initial learning rate')
+    parser.add_argument('--resume', action='store_true', default=False, help='resume training')
+    parser.add_argument('--epoch', type=int, default=300, help='training epoch')
+    parser.add_argument('--save_epoch', type=int, default=20, help='save model epoch')
+    args = parser.parse_args()
+
+    # 模型
+    net = None
+
+    # 数据
+    training_loader = None
+    test_loader = None
+
+    # 损失函数
+    loss_function = nn.CrossEntropyLoss()
+
+    # 优化函数
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+    # 不同epoch阶段，使用不同学习率
+    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[60, 120, 160], gamma=0.2)
+
+    iter_per_epoch = len(training_loader)
+
+    # 
+    warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
+
+    # 保存路径
+    checkpoint_path = None
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    model_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
+
+    # use tensorboad
+    epochs = args.epoch 
+    best_acc = 0.0
+    for epoch in range(1, epochs):
+        if epoch > args.warm:
+            train_scheduler.step(epoch)
+        
+        train(epoch)
+        acc = eval_training(epoch)
+
+        if epoch > 120 and best_acc < acc:
+            torch.save(net.state_dict(), model_path.format(net=args.net, epoch=epoch, type='best'))
+            best_acc = acc
+            continue
+        
+        if not epoch % args.save_epoch:
+            torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='regular'))
+    
+        
+
